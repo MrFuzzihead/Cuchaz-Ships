af1d4ae0e2e9deb17800fe550ea1ccd423443283
Jerome226 <commandsblocks@gmail.com>
2022-10-10 01:45:33 -0400

    Update CoreModTransformer.java

--- src/main/java/cuchaz/ships/core/CoreModTransformer.java
+++ src/main/java/cuchaz/ships/core/CoreModTransformer.java
@@ -37,7 +37,7 @@ public class CoreModTransformer implements IClassTransformer {
 		
 		try {
 			// don't transform some important stuff
-			List<String> privilegedPackages = Arrays.asList("cuchaz.ships.", "cuchaz.modsShared", "net.minecraftforge.", "cpw.", "makamys.mixingasm.", "codechicken.nei.", "bq_standard.", "eu.ha3.matmos.", "technicianlp.reauth.", "betterquesting.", "com.falsepattern.", "it.unimi.dsi.", "stanhebben.zenscript.");
+			List<String> privilegedPackages = Arrays.asList("cuchaz.ships.", "cuchaz.modsShared", "net.minecraftforge.", "cpw.", "makamys.mixingasm.", "codechicken.nei.", "bq_standard.", "eu.ha3.matmos.", "technicianlp.reauth.", "betterquesting.", "com.falsepattern.", "it.unimi.dsi.", "stanhebben.zenscript.", "org.embeddedt.");
 			for (String privilegedPackage : privilegedPackages) {
 				if (name.startsWith(privilegedPackage)) {
 					return classData;
