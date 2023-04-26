1e01be5ec73215d29857270bc20e9b469cb3383c
makamys <makamys@outlook.com>
2023-04-27 00:55:33 +0200

    Remove ref to FMLDeobfuscatingRemapper in ObfuscationAwareAdapter
    Accessing the map is completely unnecessary, since the transformer runs
    post-deobfuscation (in the SRG namespace), so mapping names to Notch names makes
    no sense.
    
    The old code effectively caused idMethodName to get returned for methods in
    Minecraft classes, and an empty string for modded classes. This commit emulates
    this weird behavior to not risk breaking any code that relies on it.
    
    Fixes incompatibility with CoreTweaks's transformer cache, which causes the map
    to not get populated (this manifested as an issue with EFR's enchanting table.)

--- src/main/java/cuchaz/ships/core/ObfuscationAwareAdapter.java
+++ src/main/java/cuchaz/ships/core/ObfuscationAwareAdapter.java
@@ -10,12 +10,7 @@
  ******************************************************************************/
 package cuchaz.ships.core;
 
-import java.lang.reflect.Field;
-import java.util.Map;
-
 import org.objectweb.asm.ClassVisitor;
-
-import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 
 public class ObfuscationAwareAdapter extends ClassVisitor {
 	
@@ -34,49 +29,14 @@ public class ObfuscationAwareAdapter extends ClassVisitor {
 	public void setPreviousClassVisitor(ClassVisitor val) {
 		cv = val;
 	}
-	
+
 	protected String getRuntimeMethodName(String runtimeClassName, String clearMethodName, String idMethodName) {
 		if (m_isObfuscatedEnvironment) {
-			return methodMapReverseLookup(getMethodMap(runtimeClassName), idMethodName);
+			/* Ideally we should just return idMethodName, but the original code behaved like this, and I don't want
+			   to deal with fixing what changing it might break. */
+			return runtimeClassName.startsWith("net/minecraft/") ? idMethodName : "";
 		} else {
 			return clearMethodName;
 		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	private Map<String,String> getMethodMap(String obfuscatedClassName) {
-		// sadly, the method maps are private
-		// let's fix that
-		try {
-			Field field = FMLDeobfuscatingRemapper.class.getDeclaredField("methodNameMaps");
-			field.setAccessible(true);
-			
-			return ((Map<String,Map<String,String>>)field.get(FMLDeobfuscatingRemapper.INSTANCE)).get(obfuscatedClassName);
-		} catch (Exception ex) {
-			throw new Error("Unable to access FML's deobfuscation mappings!", ex);
-		}
-	}
-	
-	private String methodMapReverseLookup(Map<String,String> methodMap, String idMethodName) {
-		// did we not get a method map? just pass through the method name
-		if (methodMap == null) {
-			return idMethodName;
-		}
-		
-		// methodNameMaps = Map<obfuscated class name,Map<obfuscated method name + signature,id method name>>
-		for (Map.Entry<String,String> entry : methodMap.entrySet()) {
-			if (entry.getValue().equals(idMethodName)) {
-				String obfuscatedName = entry.getKey();
-				
-				// chop off the signature
-				// ie, turn "a(III)V" into just "a"
-				
-				return obfuscatedName.substring(0, obfuscatedName.indexOf("("));
-			}
-		}
-		
-		// no method was found
-		// return empty string so it fails comparisons with expected values, but doesn't throw exceptions
-		return "";
 	}
 }
